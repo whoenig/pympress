2e6dc346e01cb311db1b35287f05c07cb5dc8604
Cimbali <me@cimba.li>
2019-11-15 03:00:15 +0100

    Moved docs to gh-pages branch

---
+++ _static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'b&#39;0.6-421-gdd6436a&#39;',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
