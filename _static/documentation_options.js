14c24193297611df030bfd79fec03e47e892313b
Cimbali <me@cimba.li>
2020-04-10 17:50:30 +0000

    Github Action-built docs update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'b&#39;0.6-505-g62e0976&#39;',
+    VERSION: 'b&#39;0.6-506-g1893a50&#39;',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
